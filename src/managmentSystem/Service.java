09305943fc0365175473ae681cfc0b600f534ea6
cm17g13 <callummcgregor50@gmail.com>
2018-04-18 16:47:54 +0100

    Changed to Gson

--- src/managmentSystem/Service.java
+++ src/managmentSystem/Service.java
@@ -8,68 +8,64 @@ import com.google.gson.Gson;
 
 public class Service {
 
-	 private Map<Integer, Account> accounts = new HashMap<Integer, Account>();  
-	 private Gson gson = new Gson(); 
-	
-	public static void main(String[] args) {
-		//empty for now, was used during testing
-	}
-	
+	private Map<Integer, Account> accounts = new HashMap<Integer, Account>();
+	private Gson gson = new Gson();
+
 	public int generateAccountNumber() {
-		
+
 		Random rand = new Random();
 		int number;
 		Account exists;
-		
+
 		do {
 			number = rand.nextInt(10000) + 1;
 			exists = accounts.get(number);
 		} while (exists != null);
-		
+
 		return number;
 	}
-	
+
 	public String convertToJson() {
-		
-		String json = gson.toJson(accounts); 
+
+		String json = gson.toJson(accounts);
 		return json;
 	}
-	
-	public Map<Integer,Account> convertToObject(String stringMap) {
-		
-		Map<Integer,Account> newMap = gson.fromJson(stringMap, HashMap.class); 
+
+	public Map<Integer, Account> convertToObject(String stringMap) {
+
+		Map<Integer, Account> newMap = gson.fromJson(stringMap, HashMap.class);
 		return newMap;
-		
+
 	}
-	
+
 	public Map<Integer, Account> getMap() {
 		return accounts;
 	}
-	
+
 	public boolean addAccount(Account account) {
-		
-		Account exists = accounts.get(account.getAccountNumber());	
-		if(exists != null) {
+
+		Account exists = accounts.get(account.getAccountNumber());
+		if (exists != null) {
 			return false;
 		} else {
 			accounts.put(account.getAccountNumber(), account);
 			return true;
 		}
 	}
-	
+
 	public Account getAccount(int accountNumber) {
 		Account exists = accounts.get(accountNumber);
-		if(exists != null) {
+		if (exists != null) {
 			return exists;
 		} else {
 			return null;
 		}
 	}
-	
+
 	public int addPadding(int number) {
 		String newNum = String.valueOf(number);
-		for(int i = newNum.length(); i < 5; i++) {
-			newNum = "0" + newNum; 
+		for (int i = newNum.length(); i < 5; i++) {
+			newNum = "0" + newNum;
 		}
 		return Integer.valueOf(newNum);
 	}
